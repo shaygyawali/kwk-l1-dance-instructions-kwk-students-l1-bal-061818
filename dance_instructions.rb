9149bb30ebcf001bd4ae7c7ec5ce6091a5bccae7
shaygyawali <shaygyawali@gmail.com>
2018-06-19 14:12:29 +0000

    automatically backed up by learn

--- dance_instructions.rb
+++ dance_instructions.rb
@@ -1,21 +1,21 @@
 
 # Code your methods below
 def gagnam_style
-def skip_step("starting_foot")
-  puts "Lower #{starting_foot} to the ground"
-  puts "Bounce #{starting_foot} back up slightly, kicking it a few inches back"
-  puts "Left, Right, Left and Left"
-end
+  def skip_step("starting_foot")
+    puts "Lower #{starting_foot} to the ground"
+    puts "Bounce #{starting_foot} back up slightly, kicking it a few inches back"
+    puts "Left, Right, Left and Left"
+  end
 
-def starting_stance
-  puts "Plant legs far apart, bend knees slightly and keep posture loose"
-end
+  def starting_stance
+    puts "Plant legs far apart, bend knees slightly and keep posture loose"
+  end
 
-def base_footwork
-  puts "Lift right foot"
-  puts "Return right foot to the ground"
+  def base_footwork
+    puts "Lift right foot"
+    puts "Return right foot to the ground"
   skip_step("Left foot")
-end
+  end
 end
 
 gangnam_style
